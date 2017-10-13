713be063d921c3b671394c8d428d138be6e4a7e5
4SkyNet <nemiroff.den@gmail.com>
2017-10-13 16:57:35 +0300

    fix: nips-learning-to-run app

--- nips-learning-to-run/environment/training.py
+++ nips-learning-to-run/environment/training.py
@@ -20,18 +20,21 @@ class Training(TrainingBase):
     def episode(self, number):
         # get initial state from environment
         observation = self.env.reset(difficulty=0)
-        print('initial observation: ', observation)
+        # print('initial observation: ', observation)
         # get first action from agent
         action = self.agent.update(reward=None, state=observation)
-        print('action: %s' % str(action))
+        # print('action: %s' % str(action))
         # update agent with state and reward
         episode_reward = 0
         for step in range(self.steps):
             observation, reward, done, info = self.env.step(action)
-            print('done: ', done)
+            # print('done: ', done)
             action = self.agent.update(reward=reward, state=observation, terminal=done)
-            log.info('step: %s, reward: %s' % (step, reward))
+            # log.info('step: %s, reward: %s' % (step, reward))
             episode_reward += reward
+            if done:
+                log.info('Episode %d (%s steps) reward: %.2f' % (number, step, episode_reward))
+                break
         # return episode reward for it to be stored to metrics as game_score
         return episode_reward
 
